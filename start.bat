e4e2165e99265f64049a57c667c7eb9a316291fd
honghulong <13027043@qq.com>
2026-06-02 22:22:18 +0800

    1
    2

--- start.bat
+++ start.bat
@@ -12,15 +12,20 @@ if not exist "%JAR_FILE%" (
 
 echo [INFO] Stopping any existing Java process...
 taskkill /f /im java.exe >nul 2>&1
-timeout /t 2 /nobreak >nul
+echo [INFO] Waiting for port %SERVER_PORT% to be released...
+timeout /t 5 /nobreak >nul
+
+set JAVA_HOME=C:\JAVA\jdk21.0.11-win_x64
+
+set SERVER_PORT=9099
 
 echo [INFO] Using Java: %JAVA_HOME%\bin\java
 echo [INFO] Starting: %JAR_FILE%
-echo [INFO] URL: http://localhost:8080/admin3
+echo [INFO] URL: http://localhost:%SERVER_PORT%/admin3
 echo [INFO] Press Ctrl+C to stop
 echo.
 
-"%JAVA_HOME%\bin\java" -jar "%JAR_FILE%" -Dspring.datasource.url=jdbc:mysql://127.0.0.1:3306/admin3?characterEncoding=utf8 -Dspring.datasource.username=root -Dspring.datasource.password=123456
+"%JAVA_HOME%\bin\java" -jar "%JAR_FILE%" --server.port=%SERVER_PORT% -Dspring.datasource.url=jdbc:mysql://127.0.0.1:3306/admin3?characterEncoding=utf8 -Dspring.datasource.username=root -Dspring.datasource.password=123456
 
 echo.
 echo [INFO] Service stopped
